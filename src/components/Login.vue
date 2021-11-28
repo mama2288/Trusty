dcc514ccc042e28a71b6d2ed8ac9d1d0a6998a7e
mama2288 <83466177+mama2288@users.noreply.github.com>
2021-11-28 18:05:07 +0700

    edit2 f

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,4 +1,55 @@
 <template>
-  <div>
-    <inp>
+  <div id="login">
+    <div class="container">
+      <div class="logo">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+      <h1>Login</h1>
+
+      <input type="email" placholder="Email" v-model="email" />
+      <br />
+      <input type="password" placholder="Password" v-model="password" />
+      <br />
+      <button @click="login">Login</button>
+      <p>
+        Need an account?
+        <router-link to="/signup"></router-link>
+      </p>
+    </div>
+  </div>
 </template>
+
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login: function () {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          (user) => {
+            console.log(user.data);
+          },
+          (err) => {
+            alert(err);
+          }
+        );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
